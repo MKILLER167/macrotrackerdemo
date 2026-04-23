a28a43a2a753a048dfb229bbe5ac76b36c165585
Anas Mo'men <1022066.anas@stemegypt.edu.eg>
2026-04-23 22:08:05 +0200

    hi

---
+++ lib/screens/screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class book extends StatelessWidget {
+  const book({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.red),
+    );
+  }
+}
